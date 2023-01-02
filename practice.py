a491624f41c54e88663c30c5f6bd03a96b1eeb89
Scriptize <dblaylock8915@gmail.com>
2023-01-02 03:15:44 -0500

    Update practice.py

--- practice.py
+++ practice.py
@@ -8,15 +8,16 @@ date_list = initial.split("/")
 
 new_date = "{}/{}/{}".format(date_list[2],date_list[1],date_list[0])
 
-#print(new_date)
+
 
 
 data = {
-  "Date": ["1980-12-02", "1980-12-03","1980-12-04","1980-12-05"],
-  "Open": [0.179688,0.177734,0.181641,0.175781],
+  "Date": ["1980/12/02", "1980/12/03","1980/12/04","1980/12/05"],
+  "Open":[0.179688,0.177734,0.181641,0.175781],
   "High": [0.183594,0.177734,0.183594,0.175781],
   "Low": [0.1835940,.177734,0.183594,0.175781]
 }
+
 #print(data["Date"])
 # Loop thru the dates
 # Format the dates
@@ -49,8 +50,14 @@ for x in fruits:
 # banana
 # cherry
 
+for x in fruits:
+  new_list.append(x)
 # Create a function that accepts a list as a parameter,loops through each date in the list, reformats it, and returns a new list
 
 
 def new_dates(date_list): # I named the function and the parameter for you; treat date_list as a list throughout the function
-  
+  new_list = []
+  for date in date_list:
+    #xyz
+    #append it to final
+  return final
