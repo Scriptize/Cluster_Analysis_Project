031c5b2d2a70f46c4258c776e3a00f2e62b47dc6
Scriptize <dblaylock8915@gmail.com>
2023-01-09 00:10:06 -0500

    testing
    *

--- practice.py
+++ practice.py
@@ -1,14 +1,11 @@
 import pandas
+from pprint import pprint
 
 initial = "1980/12/02"
 
 date_list = initial.split("/")
 
-
-
 new_date = "{}/{}/{}".format(date_list[2],date_list[1],date_list[0])
-
-
 
 
 data = {
@@ -18,3 +15,19 @@ data = {
   "Low": [0.1835940,.177734,0.183594,0.175781]
 }
 
+#Make a function that takes in a list as a parameter and reformats the dates in the list and returns the reformatted dates
+
+
+def reformater(dates):
+  final=[] #initialize final list
+  for x in dates: #loop thru date list
+    split_date = x.split("/") # split each date by the slash
+    new_date = "{}/{}/{}".format(split_date[2],split_date[1],split_date[0]) # reformat the date
+    final.append(new_date) # add the new date to the final list
+  return final
+
+
+
+data["Date"] = reformater(data["Date"]) # reformats the values in the list value of the Date key
+
+pprint(data)
